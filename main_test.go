3ada8afcadc18d4c3250635e0414436223c14f49
Torben Honigbaum <torben.honigbaum@neuland-bfi.de>
2021-05-28 15:26:38 +0200

    Additional router tests

--- main_test.go
+++ main_test.go
@@ -22,15 +22,36 @@ func TestHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", res.StatusCode, http.StatusOK)
 	}
 
+	checkReponseBody(res, t, "Welcome to Birdpedia!")
+}
+
+func TestRouterForNonExistingRoute(t *testing.T) {
+	router := newRouter()
+
+	mockServer := httptest.NewServer(router)
+
+	res, err := http.Post(mockServer.URL+"/welcome", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("status code should be 405, got %d", res.StatusCode)
+	}
+
+	checkReponseBody(res, t, "")
+}
+
+func checkReponseBody(res *http.Response, t *testing.T, expected string) {
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	expected := `Welcome to Birdpedia!`
 	actual := string(body)
 	if actual != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+		t.Errorf("handler returned unexpected body: got '%v' want '%v'", expected, actual)
 	}
 }
